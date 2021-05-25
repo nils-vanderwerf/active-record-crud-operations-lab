a71f9b167079eb178a5f4e4e3871bbb155919c6c
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-25 22:56:59 +1000

    Migrate database

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/artists.sqlite"
 )
 
 require_all 'app'
